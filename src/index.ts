ab361fa67479ab42574c660dbafa5221afe5398d
Zaahiid <zahidrsheikh01@gmail.com>
2026-02-26 19:57:20 +0530

    feat: add cookie-parser and cors middleware, enhance server initialization and health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,52 @@
-import express from "express";
+import "dotenv/config";
+import path from "path";
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import passport from "passport";
+import { Env } from "./config/env.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { HTTPSTATUS } from "./config/http.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import connectDatabase from "./config/database.config";
+import { initializeSocket } from "./lib/socket";
+import routes from "./routes";
+
+import "./config/passport.config";
+
 
 
 const app = express();
+const server = http.createServer(app);
 
-app.listen(3000, () => {
-    console.log(`Server is listening on 3000`)
+initializeSocket(server);
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: Env.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(passport.initialize());
+
+app.get(
+  "/health",
+  asyncHandler(async (req: Request, res: Response) => {
+    res.status(HTTPSTATUS.OK).json({
+      message: "Server is healthy",
+      status: "OK",
+    });
+  })
+);
+
+app.use("/api", routes);
+
+app.listen(Env.PORT, async () => {
+    await connectDatabase();
+    console.log(`Server runnig on port ${Env.PORT} in ${Env.NODE_ENV} mode`)
 })
