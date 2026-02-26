f4243442c311f2564f3ca3082f1686b4fab7f672
Zaahiid <zahidrsheikh01@gmail.com>
2026-02-26 19:44:28 +0530

    feat: implement chat functionality with controllers, services, models, routes, and socket integration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./auth.route";
 import userRoutes from "./user.route";
+import chatRoutes from "./chat.route";
 
 const router = Router();
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
+router.use("/chat", chatRoutes);
 
 export default router;
